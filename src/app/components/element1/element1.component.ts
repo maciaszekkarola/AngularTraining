cc546e1fe3d920c49b9e7d6d135499a27aa6330d
Karolina <carolha.es@gmail.com>
2017-08-31 13:14:29 +0200

    placeholderApi conditions apply

--- src/app/components/element1/element1.component.ts
+++ src/app/components/element1/element1.component.ts
@@ -8,8 +8,32 @@ import { DataService } from '../../services/data.service';
 })
 export class Element1Component implements OnInit {
   posts: Post[];
+  button1 = false;
+  button2 = false;
+  button3 = false;
 
   constructor(private dataService: DataService) { }
+
+  getFirst(event) {
+    console.log(this.button1);
+    this.button1 = !this.button1;
+    this.button2 = false;
+    this.button3 = false;
+  }
+
+  getTitles(event) {
+    console.log(this.button2);
+    this.button2 = !this.button2;
+    this.button1 = false;
+    this.button3 = false;
+
+  }
+  getRecent(event) {
+    console.log(this.button3);
+    this.button3 = !this.button3;
+    this.button2 = false;
+    this.button1 = false;
+  }
 
   ngOnInit() {
     this.dataService.getPosts().subscribe((posts) => {
