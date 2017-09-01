0694fa9966932f44fa186f1f1e9538aeb732f157
Karolina <carolha.es@gmail.com>
2017-09-01 15:30:47 +0200

    tea component ngclass tea selection

--- src/app/components/aside-left/fourth/tea/tea.component.ts
+++ src/app/components/aside-left/fourth/tea/tea.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tea.component.css']
 })
 export class TeaComponent implements OnInit {
+  classes = {
+    green: false,
+    red: true,
+    black: false,
+    white: false
+  };
+  buttons = ['matcha', 'roybos', 'ceylon', 'china'];
 
   constructor() { }
 
